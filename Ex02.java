38a430da552baedd72dfd2e73234baa97e24afc6
Mahesh Divate <95908644+maheshdivate@users.noreply.github.com>
2022-02-11 18:04:51 +0530

    Create Ex02.java

---
+++ Ex02.java
@@ -0,0 +1,7 @@
+class Ex02 {
+    public static void main(String[] args) {
+       System.out.println("Apple123");
+        for(int i =0; i < args.length;i++)
+            System.out.println(args[i]); 
+    }
+}
